c411f1ea7b85212d56a6bd2b9fb9f16a482b37f6
Yoav Gross <yoav@comedy.com>
2015-10-19 17:29:00 +0300

    temp

--- FacebookLogin/ViewController.m
+++ FacebookLogin/ViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <Foundation/Foundation.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
+
 
 @interface ViewController ()
 
@@ -17,11 +22,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self FCMloginUserInViewController:self WithCompletionHandler:^(BOOL seccess, NSString *token, NSError *loginError) {
+        NSLog(@"%@", loginError.description);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)FCMloginUserInViewController:(UIViewController *)controller
+               WithCompletionHandler:(void(^)(BOOL seccess, NSString *token, NSError *loginError))handler
+{
+    FBSDKAccessToken *token = [FBSDKAccessToken currentAccessToken];
+    if (token) {
+        
+        handler(YES, token.tokenString, nil);
+    }else{
+        //[FBSDKLoginManager renewSystemCredentials:^(ACAccountCredentialRenewResult result, NSError *error) {
+        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+        [login logInWithReadPermissions:@[@"public_profile", @"user_friends", @"user_birthday", @"user_likes", @"email"]
+                     fromViewController:controller
+                                handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+                                    if (error) {
+                                        // Process error
+                                        handler(NO, nil, error);
+                                    }else if (result.isCancelled){
+                                        // Handle cancellations
+                                        handler(NO, nil, error);
+                                    }else{
+                                        // If you ask for multiple permissions at once, you
+                                        // should check if specific permissions missing
+                                        FBSDKAccessToken *token = [FBSDKAccessToken currentAccessToken];
+                                        handler(YES, token.tokenString, nil);
+                                    }
+                                }];
+        //}];
+    }
 }
 
 @end
